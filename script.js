62d39863d7245c4f6ec67175bb7e9f668748d0b6
Shubham277353 <golubhandari405@gmail.com>
2025-08-29 19:23:53 +0530

    feat: add day-night feature

--- script.js
+++ script.js
@@ -105,32 +105,46 @@ function displayLibrary(books = myLibrary) {
   });
 }
 
-const searchBar = document.querySelector("#search-bar")
-searchBar.addEventListener("input",(e)=>{
+const searchBar = document.querySelector("#search-bar");
+searchBar.addEventListener("input", (e) => {
   const query = e.target.value.toLowerCase();
 
-  const filteredBooks = myLibrary.filter(books => 
-    books.title.toLowerCase().includes(query)||
-    books.authorName.toLowerCase().includes(query)
+  const filteredBooks = myLibrary.filter(
+    (books) =>
+      books.title.toLowerCase().includes(query) ||
+      books.authorName.toLowerCase().includes(query)
   );
 
   displayLibrary(filteredBooks);
-
 });
 
 const sortBy = document.querySelector("#sort-by");
 
-sortBy.addEventListener("change", function(){
+sortBy.addEventListener("change", function () {
   const value = this.value;
-  
-  const sortedBooks = myLibrary.filter(books => 
-    books.isRead.includes(value)
-  );
+
+  const sortedBooks = myLibrary.filter((books) => books.isRead.includes(value));
 
   displayLibrary(sortedBooks);
+});
 
-})
+const toggleBtn = document.querySelector(".toggle-theme");
 
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+  if (document.body.classList.contains("dark-mode")) {
+    toggleBtn.innerHTML = `<svg id="search-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-sun-icon lucide-sun"><circle cx="12" cy="12" r="4"/><path d="M12 2v2"/><path d="M12 20v2"/><path d="m4.93 4.93 1.41 1.41"/><path d="m17.66 17.66 1.41 1.41"/><path d="M2 12h2"/><path d="M20 12h2"/><path d="m6.34 17.66-1.41 1.41"/><path d="m19.07 4.93-1.41 1.41"/></svg>`;
+  } else {
+    toggleBtn.innerHTML = `<svg id="search-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                    class="lucide lucide-moon-star-icon lucide-moon-star">
+                    <path d="M18 5h4" />
+                    <path d="M20 3v4" />
+                    <path
+                        d="M20.985 12.486a9 9 0 1 1-9.473-9.472c.405-.022.617.46.402.803a6 6 0 0 0 8.268 8.268c.344-.215.825-.004.803.401" />
+                </svg>`;
+  }
+});
 
 function removeBook(id) {
   const index = myLibrary.findIndex((book) => book.id === id);
@@ -176,9 +190,7 @@ closeButton.addEventListener("click", () => {
   dialog.close();
 });
 
-function search(){
-
-}
+function search() {}
 
 displayLibrary();
 
